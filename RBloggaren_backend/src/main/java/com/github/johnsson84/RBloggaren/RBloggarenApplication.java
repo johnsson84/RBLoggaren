6e5d204a6f1bcbf3f650fa6e82d61e97b0c77198
Johan Johnsson <johan@johnsson-net.se>
2024-06-29 12:19:44 +0200

    init

---
+++ RBloggaren_backend/src/main/java/com/github/johnsson84/RBloggaren/RBloggarenApplication.java
@@ -0,0 +1,13 @@
+package com.github.johnsson84.RBloggaren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBloggarenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBloggarenApplication.class, args);
+	}
+
+}
---
+++ RBloggaren_backend/src/test/java/com/github/johnsson84/RBloggaren/RBloggarenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.johnsson84.RBloggaren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RBloggarenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
