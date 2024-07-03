b502b88d62e916adf56bd8f5d2e75e34f827d5c5
Johan Johnsson <johan@johnsson-net.se>
2024-07-03 14:44:27 +0200

    login function works

--- RBloggaren_frontend/src/pages/startpage/Startpage.jsx
+++ RBloggaren_frontend/src/pages/startpage/Startpage.jsx
@@ -1,25 +1,72 @@
 // Stuff
-import './Startpage.css';
-import { useState } from 'react';
+import { json } from "react-router-dom";
+import "./Startpage.css";
+import { useState } from "react";
 
 const Startpage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <div className='startpage'>
-            <h1>Rese- och Bränsleloggaren</h1>
-            <form className='form-box'>
-                <h2>Login</h2>
-                <label for="username">Username:</label><br></br>
-                <input type="text" id="username" placeholder='ex: bosse69'></input><br></br>
-                <label for="password">Password:</label><br></br>
-                <input type="text" id="password" placeholder='ex: Cheva%58'></input><br></br>
-                <div className='login-box'>
-                    <button type="submit" className="login-button">Sign In</button>
-                </div>
-                
-            </form>
+  const login = async (e) => {
+    e.preventDefault();
+
+    if (username === "" || password === "") {
+        alert("Fyll i användarnamn och lösenord")
+        return
+    };
+
+    var options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+      };
+
+    try {
+        const response = fetch(`${import.meta.env.VITE_API_URL}/auth/signin`, options);
+        const data = (await response).json();
+        console.log(data);
+
+    } catch (err) {
+        console.log("Connection error: " + err)
+    }
+  };
+
+  return (
+    <div className="startpage">
+      <h1>Rese- och Bränsleloggaren</h1>
+      <form className="form-box">
+        <h2>Login</h2>
+        <label>Användarnamn:</label>
+        <br></br>
+        <input
+          type="text"
+          id="username"
+          placeholder="ex: bosse69"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <br></br>
+        <label>Lösenord:</label>
+        <br></br>
+        <input
+          type="text"
+          id="password"
+          placeholder="ex: Cheva%58"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <br></br>
+        <div className="login-box">
+          <button type="submit" className="login-button" onClick={login}>
+            Sign In
+          </button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
 export default Startpage;
